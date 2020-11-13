e3a6ab73c41c9ddf9fe28b3e1631300ae10a0aab
liunix <liuyifan.com@gmail.com>
2020-11-13 20:43:29 +0800

    add yaml

---
+++ source/yaml1.d
@@ -0,0 +1,22 @@
+/+ dub.sdl:
+    name "yaml1"
+    dependency "dyaml" version="0.8.3"
++/
+import std.stdio;
+import dyaml;
+
+void main(string[] args)
+{
+    Node root = Loader.fromString("
+Hello World : [Hello, World]
+Answer : 42
+").load();
+    foreach(string word; root["Hello World"])
+    {
+        writeln(word);
+    }
+    writeln("The answer is ", root["Answer"].as!int);
+
+    //Dump the loaded document to output.yaml.
+    // dumper().dump(File("output.yaml", "w").lockingTextWriter, root);
+}
